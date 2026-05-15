459bb96ae8e7dc083f09bd2901356f96aaacbd16
raul_costas <raulcostasdel@gmail.com>
2026-05-14 21:54:13 -0400

    Production: Add clinicacodel.com to CORS whitelist

--- codel-backend-main/src/main.ts
+++ codel-backend-main/src/main.ts
@@ -11,7 +11,11 @@ import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
+    origin: [
+      'https://codel-frontend.onrender.com',
+      'https://clinicacodel.com',
+      'https://www.clinicacodel.com'
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
